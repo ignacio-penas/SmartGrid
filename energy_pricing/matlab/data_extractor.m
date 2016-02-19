d152ca5367d9c4a93c85ae9999f8768b907b0142
CloudAtlas13 <ignacio.penas4b@gmail.com>
2016-02-19 11:42:17 +0100

    Actualizacion data_extractor

--- energy_pricing/matlab/data_extractor.m
+++ energy_pricing/matlab/data_extractor.m
@@ -1,26 +1,26 @@
 
-cd /home/nacho/tfg/data_warehouse/energy_pricing/www.omie.es/datosPub/marginalpdbc
+function [dia] = data_extractor(dir)
 
-fdat=fopen('marginalpdbc_20140109.1', 'r');
-dia =  [];zeros(25,25);
+    cd /home/nacho/tfg/data_warehouse/energy_pricing/www.omie.es/datosPub/marginalpdbc
 
-for i = 1:25
-    if i ~= 1
-        nline=fgetl(fdat); %resto de lineas
-        separate = strsplit(nline, ';');
-        separate = cell2mat(separate);
-        [y,x] = size(separate);
-        
-        for j = 1:x
-          separate(j) = str2double(separate(j));
-          dia(i,j) = separate(j);
+    fdat=fopen(dir, 'r');
+    dia = [];
+    ite = [];
+    for i = 1:25
+        if i ~= 1
+            nline=fgetl(fdat); %resto de lineas
+            separate = strsplit(nline, ';'); 
+            [y,x] = size(separate);
+
+            for j = 1:7
+                ite(j) = str2double(separate(j));
+
+            end
+            dia(i, :) = ite(1,:);
+        else
+            nline=fgetl(fdat); %primera linea
         end
-        
-        %dia(i,n_lenght)=nline
-    else
-        nline=fgetl(fdat); %primera linea
-    end
-    
-end
 
-cd /home/nacho/tfg/SmartGrid/energy_pricing/matlab
+    end
+
+    cd /home/nacho/tfg/SmartGrid/energy_pricing/matlab
