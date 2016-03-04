30cdc84a0a306cf8788d883945254c6ec7085a5b
CloudAtlas13 <ignacio.penas4b@gmail.com>
2016-03-04 10:15:39 +0100

    Up to date

--- energy_pricing/matlab/average_week.m
+++ energy_pricing/matlab/average_week.m
@@ -1,26 +1,28 @@
-function [months] = average_calc()
-
+function [week] = average_week()
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
     today = datetime;
     aux_day = datenum(today);
-    months = zeros(12,24);
+    week = zeros(52e,24);
+    week_number = 0;
     hours_avg = zeros(1,24);
+    
     for i = 22:779
         day_before = addtodate(aux_day, -i, 'day');
         day_before = datestr(day_before);
         day_before = datetime(day_before);
-        temp_month = month(day_before);
+        temp_day = day(day_before,'dayofweek');
+        week_number = week(day_before);
         day_before = yyyymmdd(day_before);
         file_name = ['marginalpdbc_', num2str(day_before), '.1'];
         disp(file_name);
         dia = data_extractor(file_name);
         for j = 1:24
             hours_avg(j) = hours_avg(j) + dia(j+1, 6);
-            months(temp_month, j) = months(temp_month, j) + dia(j+1, 6);
+            week(temp_day, j) = week(temp_day, j) + dia(j+1, 6);
         end
     end
 
-    hours_avg = hours_avg/760;
-    for i = 1:12
-        month_days = eomday(2014, i);%Extract each month days
-        months(i, :) = months(i, :)/(2*month_days);
-    end
+
+end
+
---
+++ energy_pricing/matlab/plot_prices.m
@@ -0,0 +1,21 @@
+%calc = average_calc();
+hours = 1:24;
+months = 1:12;
+iterate_month = 1;
+%%
+%Trace 3d plot
+
+while iterate_month < 13
+    
+    figure
+    subplot(2, 1, 1);
+    plot(hours, calc(iterate_month,:));
+
+    subplot(2, 1, 2);
+    plot(hours, calc(iterate_month + 1,:));
+    
+    iterate_month = iterate_month + 2;
+end
+
+figure
+plot(hours, hours_avg);
