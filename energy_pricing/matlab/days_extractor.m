447aafcd16cb60cd0f5510cc4c8fc6e7443983d3
CloudAtlas13 <ignacio.penas4b@gmail.com>
2016-04-01 18:47:59 +0200

    Multiple plot update

--- energy_pricing/matlab/days_extractor.m
+++ energy_pricing/matlab/days_extractor.m
@@ -1,22 +1,20 @@
 function [day] = days_extractor()
     %Extracts two years of prices in the day array
     %Almacenamos cada dia de la semana en una fila
-    day = zeros(110, 7);
+    
     number_of_days = zeros(7,1);
     today = datetime;
     aux_day = datenum(today);
-    n_day_from_31jan = floor(daysact('31-jan-2016', aux_day));
+    n_day_from_1jan = floor(daysact('1-jan-2011', aux_day));
+    day = zeros(n_day_from_1jan, 7);
     
-    for i = n_day_from_31jan : (n_day_from_31jan + 757)
+    for i = 1 : (n_day_from_1jan)
         day_before = addtodate(aux_day, -i, 'day');
         dayNumber = weekday(day_before);
         day_before = datestr(day_before);
-        %disp(day_before);
         day_before = datetime(day_before);
         day_before = yyyymmdd(day_before);
         
-        
-        disp(dayNumber);
         number_of_days(dayNumber) = number_of_days(dayNumber) + 1;
         %Recovery of the pricing file
         file_name = ['marginalpdbc_', num2str(day_before), '.1'];
