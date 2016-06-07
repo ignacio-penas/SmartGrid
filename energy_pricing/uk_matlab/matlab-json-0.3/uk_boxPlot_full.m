eda281ddcdfcf9226f89ace4c7b2cbed48f8d28f
CloudAtlas13 <ignacio.penas4b@gmail.com>
2016-06-07 17:07:41 +0200

    Balance PT and lab computer commit

--- energy_pricing/uk_matlab/matlab-json-0.3/uk_boxPlot_full.m
+++ energy_pricing/uk_matlab/matlab-json-0.3/uk_boxPlot_full.m
@@ -1,4 +1,4 @@
-function [ day, weekly_day ] = uk_boxPlot( yy2013, yy2014, yy2015 )
+function [weekly_day ] = uk_boxPlot_full( yy2013, yy2014, yy2015 )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -14,54 +14,48 @@ long3 = size(yy2015);
 long3 = double(long3(2));
 lenght = long +long2 + long3;
 weekly_day = zeros(7, lenght);
-
-
+disp(lenght);
+counter = 1;
 
 i = 1;
+disp('2013');
 while i <= long
-    disp('2013');
-    disp(i);
     tempEpoch = str2double(yy2013(i).millisUTC);
     date = unix2matlab(tempEpoch);
     date = datetime(date);
     weekDay = weekday(date);
     
-    prices_acum(weekDay) = prices_acum(weekDay) + str2double(yy2013(i).price);
-    number_of_prices(weekDay) = number_of_prices(weekDay) + 1;
+    weekly_day(weekDay,counter) = str2double(yy2013(i).price);
+    counter = counter + 1;
     i = i+1;
 end
 
 
 i = 1;
+disp('2014');
 while i <= long2
-    disp('2014');
-    disp(i);
     tempEpoch = str2double(yy2014(i).millisUTC);
     date = unix2matlab(tempEpoch);
     date = datetime(date);
-    
     weekDay = weekday(date);
     
-    prices_acum(weekDay) = prices_acum(weekDay) + str2double(yy2014(i).price);
-    number_of_prices(weekDay) = number_of_prices(weekDay) + 1;
+    weekly_day(weekDay,counter) = str2double(yy2014(i).price);
+    counter = counter + 1;
     i = i+1;
 end
 
 
 i = 1;
+disp('2015');
 while i <= long3
-    disp('2015');
-    disp(i);
     tempEpoch = str2double(yy2015(i).millisUTC);
     date = unix2matlab(tempEpoch);
     date = datetime(date);
-    
     weekDay = weekday(date);
     
-    prices_acum(weekDay) = prices_acum(weekDay) + str2double(yy2015(i).price);
-    number_of_prices(weekDay) = number_of_prices(weekDay) + 1;
+    weekly_day(weekDay,counter) = str2double(yy2015(i).price);
+    counter = counter + 1;
     i = i+1;
 end
-
-day = number_of_prices ./ prices_acum;
+disp(counter);
 
