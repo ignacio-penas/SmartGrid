19d94e3ea3e3ff28312f980a0469805df443cf69
CloudAtlas13 <ignacio.penas4b@gmail.com>
2016-06-21 13:25:51 +0200

    Results extracted already. Data reliability check left

--- workload_model/consumption_statistics/calculate_log.m
+++ workload_model/consumption_statistics/calculate_log.m
@@ -1,24 +1,27 @@
-function [ pot_partial, price_partial] = calculate_log( nCpusUsed, elapsedTime, runTime,sp_average_days )
+function [ pot_partial, price_function] = calculate_log( nCpusUsed, elapsedTime, runTime,average_days )
 %calculate_log 
 %   This function returns the average consuption of the log passed on the
-%   parameters
+%   parameters. The last parameter to be passed is a 1x24 array containing
+%   the average pricing per hours of the case to be calculated
 
 pot_cte = 153.4;
 mem_cte = 0.56;
 cpu_cte = 0.85;
 n_cpu_base = 12;
 mem_usage = 50;
-price_partial = 0;
+price_function = 0;
 unixTime = 1296571124;
 
 pot_partial = 0;
-price_partial = 0;
+price_function = 0;
 
 mem_consumption = mem_cte*mem_usage;
 cpu_consumption = cpu_cte*nCpusUsed*100/n_cpu_base;
 
-time = unix2matlab(runTime + unixTime)*1000;
+time = unix2matlab((runTime + unixTime)*1000);
 h = hour(time);
+
+
 
 aux_pot = 0;
 
@@ -29,16 +32,19 @@ module = floor(elapsedTime);
 rest = mod(elapsedTime, module);
 upper_int = ceil(elapsedTime);
 
+aux_pot = (pot_cte + mem_consumption + cpu_consumption);
 for j = 1:upper_int
     if j == upper_int
-        aux_pot = (pot_cte + mem_consumption + cpu_consumption)*rest;
+        aux_pot = aux_pot*rest;
         pot_partial = pot_partial + aux_pot;
-        price_partial = price_partial + aux_pot*(sp_average_days(1,(mod((j-1),24)+1))/1000000);
+        price_function = price_function + aux_pot*(average_days(1,(mod(((h+j)-1),24)+1))/1000000);
     else
-        aux_pot = (pot_cte + mem_consumption + cpu_consumption);
+        
         pot_partial = pot_partial + aux_pot;
-        price_partial = price_partial + aux_pot*(sp_average_days(1,(mod((j-1),24)+1))/1000000);
+        price_function = price_function + aux_pot*(average_days(1,(mod(((h+j)-1),24)+1))/1000000);
     end;
 end;
+
+end
 
 
--- workload_model/consumption_statistics/consumption_averages.m
+++ workload_model/consumption_statistics/consumption_averages.m
@@ -1,7 +1,13 @@
-function [pTotal, priceTotal] = consumption_averages(country)
-
+function [pTotal, priceTotal, price_comparation, pot_comparation] = consumption_averages(country)
+%consumption_averages this function selects the country in which we
+%calculate the energy consumption and money spent throw the CEA Curie
+%workload logs given be parallel workload webpage.
 pTotal = 0;
 priceTotal = 0;
+price_comparation = zeros(1,4);
+disp(price_comparation);
+pot_comparation = zeros(1,4);
+
 posibilities = {'sp','spain','1';
                 'uk','united kingdom','2';
                 'us','united states', '3';
@@ -16,6 +22,7 @@ for i = 1:4
         end;
     end;
 end;
+price_partial = 0;
 
 cd ~/tfg/SmartGrid/workload_model/plots
 load('diaOptimized.mat'); 
@@ -31,15 +38,58 @@ switch sw
             [ pot_partial, price_partial] = calculate_log( diaOptimized(3, i), diaOptimized(2, i),(diaOptimized(1, i)+diaOptimized(5, i)), sp_average_days);
             pTotal = pTotal + pot_partial;
             priceTotal = priceTotal + price_partial;
-            disp(i);
+            %disp(i);
         end;
         
     case 2
         disp('uk');
+        cd ~/tfg/SmartGrid/energy_pricing/comparing
+        load('uk_average_day.mat');
+        cd ~/tfg/SmartGrid/workload_model/consumption_statistics
+        for i = 1:logs_number
+            [ pot_partial, price_partial] = calculate_log( diaOptimized(3, i), diaOptimized(2, i),(diaOptimized(1, i)+diaOptimized(5, i)), uk_average_days);
+            pTotal = pTotal + pot_partial;
+            priceTotal = priceTotal + price_partial;
+            disp(i);
+        end;
     case 3
         disp('us');
+        cd ~/tfg/SmartGrid/energy_pricing/comparing
+        load('us_average_day.mat');
+        cd ~/tfg/SmartGrid/workload_model/consumption_statistics
+        for i = 1:logs_number
+            [ pot_partial, price_partial] = calculate_log( diaOptimized(3, i), diaOptimized(2, i),(diaOptimized(1, i)+diaOptimized(5, i)), us_average_days);
+            pTotal = pTotal + pot_partial;
+            priceTotal = priceTotal + price_partial;
+            disp(i);
+        end;
     case 4
         disp('compare');
+        cd ~/tfg/SmartGrid/energy_pricing/comparing
+        load('sp_average_day.mat');
+        load('uk_average_day.mat');
+        load('us_average_day.mat');
+        cd ~/tfg/SmartGrid/workload_model/consumption_statistics
+        for i = 1:logs_number
+            %%Spain
+            [ pot_partial, price_partial] = calculate_log( diaOptimized(3, i), diaOptimized(2, i),(diaOptimized(1, i)+diaOptimized(5, i)), sp_average_days);
+            pot_comparation(1,1) = pot_comparation(1,1) + pot_partial;
+            price_comparation(1,1) = price_comparation(1,1) + price_partial;
+            %%UK
+            [ pot_partial, price_partial] = calculate_log( diaOptimized(3, i), diaOptimized(2, i),(diaOptimized(1, i)+diaOptimized(5, i)), uk_average_days);
+            pot_comparation(1,2) = pot_comparation(1,2) + pot_partial;
+            price_comparation(1,2) = price_comparation(1,2) + price_partial;
+            %%US
+            [ pot_partial, price_partial] = calculate_log( diaOptimized(3, i), diaOptimized(2, i),(diaOptimized(1, i)+diaOptimized(5, i)), us_average_days);
+            pot_comparation(1,3) = pot_comparation(1,3) + pot_partial;
+            price_comparation(1,3) = price_comparation(1,3) + price_partial;
+            
+            %%Balance
+            [ pot_partial, price_partial] = balance_logs( diaOptimized(3, i), diaOptimized(2, i),(diaOptimized(1, i)+diaOptimized(5, i)),sp_average_days, uk_average_days, us_average_days);
+            pot_comparation(1,4) = pot_comparation(1,4) + pot_partial;
+            price_comparation(1,4) = price_comparation(1,4) + price_partial;
+            disp(i);
+        end;
     otherwise
         disp('bad parameter');
 end
