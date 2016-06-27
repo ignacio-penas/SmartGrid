b7665196d7a15853c3eabd7707a57750d3257b67
CloudAtlas13 <ignacio.penas4b@gmail.com>
2016-06-27 16:27:17 +0200

    Error in plot fixed

--- energy_pricing/us_pricing/plot_months_per_weeks.m
+++ energy_pricing/us_pricing/plot_months_per_weeks.m
@@ -6,7 +6,7 @@ temp = zeros(7,5,12);
 for i = 1 : 7
    for j = 1 : 5
       for k = 1 : 12
-        temp(i,j,k) = average_months_by_weeks(k,i,j);
+        temp(i,j,k) = us_average_months_by_weeks(i,j,k);
       end
    end 
 end
@@ -16,7 +16,7 @@ for i = 1 : 12
     plot(temp(:, :, i));
     title(headers(i));
     ylabel('$ MW/h');
-    legend('Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6');
+    legend('Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5');
     set(gca, 'XTickLabel',{'Sun','Mon','Tue','Wed','Thu','Fri','Sat'});
     ylim([10 50]);
 end
